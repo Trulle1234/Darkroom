a9a61a9f24b6491c363bc83b0f9f8166d5845fbc
Trulle1234 <leonard.trulsson@gmail.com>
2026-03-14 13:01:56 +0100

    first commit

---
+++ photography-ysws/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href="https://icons.hackclub.com/api/icons/0xe6e1da/camera-fill"/>
+</svelte:head>
+
+{@render children()}
---
+++ photography-ysws/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import "../app.css"
+</script>
+
+<img 
+    src="\images\flag-orpheus-left-color-fix.svg"
+    alt="Hack CLb"
+    id="flag"
+/>
+
+<h1 id="title">Darkroom</h1>
