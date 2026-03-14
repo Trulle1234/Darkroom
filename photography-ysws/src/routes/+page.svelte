aa37a411916f6e785fe7330806aa0bc394d41f3b
Trulle1234 <leonard.trulsson@gmail.com>
2026-03-14 14:54:44 +0100

    main stuff done :thumbs-up:

--- photography-ysws/src/routes/+page.svelte
+++ photography-ysws/src/routes/+page.svelte
@@ -1,11 +1,34 @@
 <script lang="ts">
-    import "../app.css"
+	import '../app.css';
+    import { onMount } from 'svelte';
+    import { blur, crossfade, draw, fade, fly, scale, slide } from 'svelte/transition';
+
+	let show = $state(false);
+
+	onMount(() => {
+		show = true;
+        console.log("loaded");
+	});
 </script>
 
-<img 
-    src="\images\flag-orpheus-left-color-fix.svg"
-    alt="Hack CLb"
-    id="flag"
-/>
+<a href="https://hackclub.com/">
+	<img
+		src="/images/flag-orpheus-left-color-fix.svg"
+		alt="Hack Club"
+		id="flag"
+	/>
+</a>
 
-<h1 id="title">Darkroom</h1>
+
+{#if show}
+    <div id="main" transition:blur={{ duration: 1000 }}>
+        <div id="hero">
+            <h1 id="title">Darkroom</h1>
+
+            <p id="description">
+                Build something photography related<br>
+                Get Cameras, Gear, film and other cool stuff!
+            </p>
+        </div>
+    </div>
+{/if}
