319fa879df8acff99238093b30d1b3776d2fd9d3
Trulle1234 <leonard.trulsson@gmail.com>
2026-03-16 12:28:22 +0100

    started work on faq and wokred on the example prizes

--- photography-ysws/src/routes/+page.svelte
+++ photography-ysws/src/routes/+page.svelte
@@ -10,23 +10,33 @@
 
 	let items = [
 		["https://cdn.hackclub.com/019cf3a4-5c6d-7085-ae8c-b6e7267c1257/instax_crop.png",
-			"Instax", "An Instax camera"],
+			"Instax", "An Instax instant camera"],
 		["https://cdn.hackclub.com/019cf3a4-5e21-7fcf-a9e9-cd2ad4f39e76/memory_cards_crop.png", 
 			"Memory cards", "Memory cards to store your photos"],
-		["https://cdn.hackclub.com/019cf3a4-6360-7cfe-abcc-f1248cd352a7/printed_photps_crop.png",
-			"Printed Photos", "A grant to print your photos"],
+		["https://cdn.hackclub.com/019cf658-bc26-752a-8278-4eabf833026d/accessories_crop.png",
+			"Accessories", "A grant for accessories"],
 		["https://user-cdn.hackclub-assets.com/019cf3ab-4853-70fe-b981-c9e504397c82/kodak_chamera_crop.png",
 			"Kodak Charmera", "Kodak Charmera - A keychain camera"],
-		// not done below here
-		["https://cdn.hackclub.com/019cf3c0-970b-7f23-8cc8-29eb8d212ea1/placeholder.png",
-			"Placeholder", "Placeholder"],
-		["https://cdn.hackclub.com/019cf3c0-970b-7f23-8cc8-29eb8d212ea1/placeholder.png",
-			"Placeholder", "Placeholder"],
-		["https://cdn.hackclub.com/019cf3c0-970b-7f23-8cc8-29eb8d212ea1/placeholder.png",
-			"Placeholder", "Placeholder"],
-		["https://cdn.hackclub.com/019cf3c0-970b-7f23-8cc8-29eb8d212ea1/placeholder.png",
-			"Placeholder", "Placeholder"],
+		["https://cdn.hackclub.com/019cf63c-c237-7edf-bedb-aa6554c0fdd9/sticker_sheet_placeholder_crop.png",
+			"Sticker sheet", "A cool sticker sheet!"],
+		["https://cdn.hackclub.com/019cf63c-be51-7b67-aa1b-aa18b0012a12/film_crop.png",
+			"Film", "Some Instax film, possibly others too"],
+		["https://cdn.hackclub.com/019cf63c-c06e-77ce-90d8-ad3a0f4e9750/digicam_crop.png",
+			"Digicam", "A Digicam, exactly which is tbd"],
+		["https://cdn.hackclub.com/019cf3a4-6360-7cfe-abcc-f1248cd352a7/printed_photps_crop.png",
+			"Printed Photos", "A grant to get your photo printed"],
+		];
+
+	let faqs = [
+		["Am I eligible to participate?", "If you are between 13 and 18 your are eligible. You will have to verify your identity to purches stuff in the shop."],
+		["Question", "Answer"],
 	];
+
+	let openIndex: number | null = $state(null);
+
+	function toggle(index: number) {
+		openIndex = openIndex === index ? null : index;
+	}
 </script>
 
 <a href="https://hackclub.com/">
@@ -56,15 +66,15 @@
 
 <div id="dark">
 	<div class:visible={show}>
-		<h6 id="get">Get stuff like:</h6>
+		<h2 id="get">Get stuff like:</h2>
 		<i id="det-disclaimer">*This is subject to change!</i>
 
 		<div id="polaroid-grid">
 			{#each items as [url, alt, desc]}
 				<div class="polaroid-wrap" style="transform: rotate(
-					{Math.random() < 0.4
-						? -(Math.random() * 5 + 5)
-						: Math.random() * 5 + 5}deg);">
+					{Math.random() < 0.5
+						? -(Math.random() * 4 + 2)
+						: Math.random() * 4 + 2}deg);">
 						
 					<img
 						src={url}
@@ -79,6 +89,25 @@
 					<p class="polaroid-text">{desc}</p>
 				</div>
 			{/each}
+		</div>
+		
+		<h2 id="faq-head">FAQ:</h2>
+
+		<div id="faq">
+			{#each faqs as [q, a], i}
+				<div class="faq-item">
+					<button onclick={() => toggle(i)}>
+					{q}
+					<span>{openIndex === i ? '▲' : '▼'}</span>
+					</button>
+					
+					{#if openIndex === i}
+					<div class="answer">
+						{a}
+					</div>
+					{/if}
+				</div>			
+  			{/each}
 		</div>
 	</div>
 </div>
